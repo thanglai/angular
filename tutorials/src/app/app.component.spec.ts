3186998ef9422b8bfcef84ada5bbe909913485f2
thanglai <thanglai69@gmail.com>
2023-07-10 10:46:04 -0400

    Update tests

--- tutorials/src/app/app.component.spec.ts
+++ tutorials/src/app/app.component.spec.ts
@@ -1,9 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
+    imports: [AppComponent],
+    providers: [
+      {
+        provide: ActivatedRoute,
+        useValue: {
+          params: of([{id: 1}]),
+        },
+      },
+    ],
   }));
 
   it('should create the app', () => {
@@ -15,13 +25,13 @@ describe('AppComponent', () => {
   it(`should have as title 'tutorials'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tutorials');
+    expect(app.title).toEqual('homes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tutorials app is running!');
+    expect(compiled.querySelector('.content')).toBeTruthy();
   });
 });
