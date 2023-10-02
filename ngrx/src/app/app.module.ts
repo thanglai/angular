991ca9fbb5137df1c95d8a65b49e4a5e3113f706
thanglai <thanglai69@gmail.com>
2023-10-02 14:13:09 -0400

    https://ngrx.io/guide/store tutorial

---
+++ ngrx/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyCounterComponent
+  ],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
